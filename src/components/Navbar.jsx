fb1c3d9b426e272c572b9811acd47423e23a1dec
TesyaKell <tesyarakhel@gmail.com>
2026-07-10 17:30:25 +0700

    info navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -71,7 +71,7 @@ export default function Navbar() {
               Tesya Rakhel
             </p>
             <p className="text-[11px] uppercase tracking-[0.2em] text-yellow-400">
-              Front-End Developer
+              SoftwareDeveloper
             </p>
           </div>
         </a>
